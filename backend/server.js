48fe3d26e3b6db8584da5488abe85d8ae49c72ea
deepakV-0001 <ddt8764156581@gmail.com>
2025-02-10 14:09:51 +0530

    final done

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: 'http://localhost:3000', 
+    origin: 'http://localhost:3000' || 'https://event-management-system-chi-five.vercel.app/' || '*' , 
     credentials: true, 
     methods: ['GET', 'POST', 'PUT', 'DELETE'], 
     allowedHeaders: ['Content-Type', 'Authorization'], 
